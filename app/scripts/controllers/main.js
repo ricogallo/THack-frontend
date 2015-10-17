941f0aec626b047fffd61008738cf4131452510a
Riccardo Gueli Alletti <riccardo.gueli.alletti@gmail.com>
2015-10-17 15:41:15 +0200

    First commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name mashopoloApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the mashopoloApp
+ */
+angular.module('mashopoloApp')
+  .controller('MainCtrl', function () {
+    this.message = 'Mashopoooolo';
+  });
