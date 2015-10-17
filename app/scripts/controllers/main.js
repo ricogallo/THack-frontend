9b3f593459a90b5f746dedaebc3553742a87a258
Riccardo Gueli Alletti <riccardo.gueli.alletti@gmail.com>
2015-10-18 00:15:36 +0200

    Added the type to the flight request

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -11,8 +11,19 @@ angular.module('mashopoloApp')
   .controller('MainCtrl', function ($scope, $rootScope, iframePath) {
     $scope.search = function(query) {
       $rootScope.positionPromise.then(function(position) {
+        var type;
+
+        if (/facebook\.com/.test(query)) {
+          type = 'facebook';
+        } else if (/eventbrite\.com/.test(query)) {
+          type = 'eventbrite';
+        } else {
+          type = 'generic';
+        }
+
         $scope.widgetPath = iframePath +
                             '?url=' + encodeURI(query) +
+                            '&type=' + type +
                             '&lat=' + position.coords.latitude +
                             '&long=' + position.coords.longitude;
         $scope.embedCode = '<iframe src="' + $scope.widgetPath + '">';
