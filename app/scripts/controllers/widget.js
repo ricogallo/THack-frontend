0ef418885f36e1d9664e9b775992670367671c96
Riccardo Gueli Alletti <riccardo.gueli.alletti@gmail.com>
2015-10-18 11:34:24 +0200

    wow

--- app/scripts/controllers/widget.js
+++ app/scripts/controllers/widget.js
@@ -8,13 +8,14 @@
  * Controller of the mashopoloApp
  */
 angular.module('mashopoloApp')
-  .controller('WidgetCtrl', function ($q, $scope, $route, flights, payments, hotels, cityFromAirport, $timeout) {
+  .controller('WidgetCtrl', function ($q, $rootScope, $scope, $route, flights, payments, hotels, cityFromAirport, $timeout) {
     $scope.airlineResults = [];
     $scope.hotelResults = [];
 
     $scope.widgetLoading = true;
 
     flights.search($route.current.params).then(function(res) {
+      $rootScope.checkinDate = res.data.flights[0].segments[0].arrivalTime;
       var hotelParams = {
         lat: $route.current.params.lat,
         long: $route.current.params.long,
@@ -29,8 +30,8 @@ angular.module('mashopoloApp')
       ])
     })
     .then(function(responses) {
-        $scope.departureCity = responses[0].data.airports[0].city;
-        $scope.arrivalCity = responses[1].data.airports[0].city;
+        $rootScope.departureCity = responses[0].data.airports[0].city;
+        $rootScope.arrivalCity = responses[1].data.airports[0].city;
         $scope.hotelResults = responses[2].data;
         $scope.airlineResults = responses[3].data;
         $scope.widgetLoading = false;
@@ -55,8 +56,6 @@ angular.module('mashopoloApp')
 
       if ($scope.is('flights')) {
         $scope.selectedFlight = null;
-        $scope.departureCity = null;
-        $scope.arrivalCity = null;
       }
       if ($scope.is('hotels')) $scope.selectedHotel = null;
       if ($scope.is('checkout')) $scope.user = {};
